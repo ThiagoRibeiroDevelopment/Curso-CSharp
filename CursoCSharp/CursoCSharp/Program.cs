9f64ffb5f3f8d278ea6e2796cf40734cf5d11413
Thiago Ribeiro <thiago.ribeiro.lemaf@gmail.com>
2020-05-08 16:29:02 -0300

    Criação do Projeto curso_C#

---
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Thiago";
+            Console.WriteLine(nome);
+            Console.WriteLine("Hello World 2!");
+        }
+    }
+}
