dadb6c23287cdc8a7771b01caf489e8e1c6fdc61
Thiago Ribeiro <thiago.ribeiro.lemaf@gmail.com>
2020-05-13 12:02:35 -0300

    Execoções e tratamento de erro, e criando exceções personalizadas

---
+++ CursoCSharp/CursoCSharp/Excecoes/PrimeiraExcecao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Excecoes
+{
+    public class Conta
+    {
+        double Saldo;
+
+        public Conta(double saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > Saldo)
+            {
+                //throw usado pra lançar a exceção
+                throw new ArgumentException("Saldo insuficiente.");
+            }
+
+            Saldo -= valor;
+        }
+    }
+
+    class PrimeiraExcecao
+    {
+        public static void Executar()
+        {
+            var conta = new Conta(1_223.45);
+            //try é usado pra metodos que podem dar algum erro, se der o erro ele chama o bloco catch
+            try
+            {
+                // int.Parse("abc");
+
+                conta.Sacar(1600);
+                Console.WriteLine("Retirada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Obrigado!");
+            }
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -7,6 +7,7 @@ using CursoCSharp.ClassesEMetodos;
 using CursoCSharp.Colecoes;
 using CursoCSharp.OO;
 using CursoCSharp.MetodosEFuncoes;
+using CursoCSharp.Excecoes;
 
 namespace CursoCSharp {
     class Program {
@@ -75,6 +76,9 @@ namespace CursoCSharp {
                 { "Delegates Como Parâmetros - Métodos & Funções", DelegatesComoParametros.Executar},
                 { "Métodos de Extensão - Métodos & Funções", MetodosDeExtensao.Executar},
 
+                // Exceções
+                { "Primeira Exceção - Exceções", PrimeiraExcecao.Executar},
+                { "Exceções Personalizadas - Exceções", ExcecoesPersonalizadas.Executar},
             }) ;
 
             central.SelecionarEExecutar();
