e859df7789565825af467db038e03a690be6ac5f
Thiago Ribeiro <thiago.ribeiro.lemaf@gmail.com>
2020-05-14 08:02:23 -0300

    Expplorando Api: Criando, lendo e movendo arquivos, trabalhando com diretorios,info de diretorios e arquivos, e trabalhando com pasta, DateTime e TimeSpan

---
+++ CursoCSharp/CursoCSharp/Api/ExemploPath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace CursoCSharp.Api
+{
+    class ExemploPath
+    {
+        public static void Executar()
+        {
+            var arquivo = @"~/exemplo_path_arquivo.txt".ParseHome();
+            var pasta = @"~/exemplo_path_pasta".ParseHome();
+
+            if (!File.Exists(arquivo))
+            {
+                using (StreamWriter sw = File.CreateText(arquivo))
+                {
+                    sw.WriteLine("Um novo arquivo criado!");
+                }
+            }
+
+            if (!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+
+            Console.WriteLine(Path.GetExtension(arquivo));//Passa a extensao do arquivo
+            Console.WriteLine(Path.GetFileName(arquivo));//Passa o nome do arquivo
+            Console.WriteLine(Path.GetFileNameWithoutExtension(arquivo));//Passa o nome do arquivo sem a extensão
+            Console.WriteLine(Path.GetDirectoryName(arquivo));//Passa o diretorio onde esta o arquivo
+            Console.WriteLine(Path.HasExtension(arquivo)); //Diz se tem extensão
+
+            Console.WriteLine(Path.HasExtension(pasta)); 
+            Console.WriteLine(Path.GetFullPath(pasta)); //retorna o caminho completo da pasta
+            Console.WriteLine(Path.GetPathRoot(pasta)); //retorna em que raiz esta a pasta
+        }
+    }
+}
---
+++ CursoCSharp/CursoCSharp/Api/ExemploTimeSpan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Api
+{
+    class ExemploTimeSpan
+    {
+        public static void Executar()
+        {
+            var intervalo = new TimeSpan(days: 10, hours: 20, minutes: 30, seconds: 40);
+            Console.WriteLine(intervalo);
+
+            Console.WriteLine("Minutos: " + intervalo.Minutes);
+            Console.WriteLine("Intervalo em Minutos: " + intervalo.TotalMinutes);
+
+            var largada = DateTime.Now;
+            var chegada = DateTime.Now.AddMinutes(15);
+
+            var tempo = chegada - largada;
+
+            Console.WriteLine("Duração: " + tempo);
+
+            Console.WriteLine(intervalo.Add(TimeSpan.FromMinutes(8))); //acrescentou 8 minuto
+            Console.WriteLine(intervalo.Subtract(TimeSpan.FromMinutes(8)));//subtrai 8 min do original, e nao do somado
+            Console.WriteLine(intervalo); //so pra mostrar que nao muda o original
+
+            Console.WriteLine("ToString 1: " + intervalo.ToString("g")); //sem os segundos
+            Console.WriteLine("ToString 2: " + intervalo.ToString("G"));//com os segundos
+            Console.WriteLine("ToString 3: " + intervalo.ToString("c"));//coloca ponto no lugar de : entre hora e min
+
+            //Converter uma string pra uma TimeSpan usando Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03").TotalMilliseconds);
+        }
+    }
+}
