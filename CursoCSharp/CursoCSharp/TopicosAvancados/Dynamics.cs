c384291272f14d072ae14e2cf3ebcbb260471fb3
Thiago Ribeiro <thiago.ribeiro.lemaf@gmail.com>
2020-06-03 08:22:54 -0300

    Conclusao do curso de C#

---
+++ CursoCSharp/CursoCSharp/TopicosAvancados/Dynamics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.TopicosAvancados
+{
+    class Dynamics
+    {
+        public static void Executar()
+        {
+            //dynamic é tipo fraco, permite mudar o tipo da variavel
+            //dynamic permite começar uma variavel de um tipo e depois mudar o tipo
+            dynamic meuObjeto = "teste";
+            meuObjeto = 3;
+
+            meuObjeto++;
+            Console.WriteLine(meuObjeto);
+            //dynamic.ExpandoObjet cria um objeto q pode ir acrescentado atributos (Expande o objeto)
+            dynamic aluno = new System.Dynamic.ExpandoObject();
+            aluno.nome = "Maria Julia";
+            aluno.nota = 8.9;
+            aluno.idade = 24;
+
+            Console.WriteLine($"{aluno.nome} {aluno.nota} {aluno.idade}");
+        }
+    }
+}
